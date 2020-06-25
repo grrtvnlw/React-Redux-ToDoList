96fee211f2d7479b15ee46fdc4b73cfab6158a21
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-06-25 13:46:52 -0400

    dependencies updated

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -8,6 +8,9 @@ class TaskList extends Component {
   render() {
     return (
       <div>
+        <button>Incomplete</button>
+        <button>Complete</button>
+        
         <h2 className="TaskList__title">Incomplete Tasks</h2>
         {this.props.tasks.map((task, i) => {
           if (!task.complete) {
