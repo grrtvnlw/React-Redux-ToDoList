b1200c8ffaf2608cff88b577013b3e860a40b0e2
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-06-25 14:12:33 -0400

    Functioning combined reducer

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,52 +1,61 @@
 import React, { Component } from 'react';
-import Task from './Task';
-import { toggleTodo, deleteTodo } from './redux/actions/todoActions';
-
 import { connect } from 'react-redux';
+
+import Task from './Task';
+
+import { toggleTodo, deleteTodo } from './redux/actions/todoActions';
+import { setVisibility } from './redux/actions/visibilityActions';
 
 class TaskList extends Component {
   render() {
     return (
       <div>
-        <button>Incomplete</button>
-        <button>Complete</button>
-        
-        <h2 className="TaskList__title">Incomplete Tasks</h2>
-        {this.props.tasks.map((task, i) => {
-          if (!task.complete) {
-            return (
-              <Task
-                task={task}
-                key={i}
-                toggleCompleteStatus={() => {
-                  this.props.toggleCompleteStatus(i);
-                }}
-                deleteTask={() => {
-                  this.props.deleteTask(i);
-                }}
-              />
-            );
-          }
-          return '';
-        })}
-        <h2 className="TaskList__title">Complete Tasks</h2>
-        {this.props.tasks.map((task, i) => {
-          if (task.complete) {
-            return (
-              <Task
-                task={task}
-                key={i}
-                toggleCompleteStatus={() => {
-                  this.props.toggleCompleteStatus(i);
-                }}
-                deleteTask={() => {
-                  this.props.deleteTask(i);
-                }}
-              />
-            );
-          }
-          return '';
-        })}
+        <button onClick={() => {this.props.setVisibility('incomplete')}}>Incomplete</button>
+        <button onClick={() => {this.props.setVisibility('complete')}}>Complete</button>
+        { this.props.visibility === 'incomplete' && (
+          <>
+          <h2 className="TaskList__title">Incomplete Tasks</h2>
+          {this.props.tasks.map((task, i) => {
+            if (!task.complete) {
+              return (
+                <Task
+                  task={task}
+                  key={i}
+                  toggleCompleteStatus={() => {
+                    this.props.toggleCompleteStatus(i);
+                  }}
+                  deleteTask={() => {
+                    this.props.deleteTask(i);
+                  }}
+                />
+              );
+            }
+            return '';
+          })}
+          </>
+        )}
+        { this.props.visibility === 'complete' && (
+          <>
+          <h2 className="TaskList__title">Complete Tasks</h2>
+          {this.props.tasks.map((task, i) => {
+            if (task.complete) {
+              return (
+                <Task
+                  task={task}
+                  key={i}
+                  toggleCompleteStatus={() => {
+                    this.props.toggleCompleteStatus(i);
+                  }}
+                  deleteTask={() => {
+                    this.props.deleteTask(i);
+                  }}
+                />
+              );
+            }
+            return '';
+          })}
+        </>
+        )}
       </div>
     );
   }
@@ -54,13 +63,15 @@ class TaskList extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    tasks: state.todoState.tasks
+    tasks: state.todoState.tasks,
+    visibility: state.visibility
   }
 }
 
 const mapDispatchToProps = {
   toggleCompleteStatus: toggleTodo,
   deleteTask: deleteTodo,
+  setVisibility: setVisibility
 }
 
 export default connect(
